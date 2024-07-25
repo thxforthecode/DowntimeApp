0e92c7d817d008b9f3324710181d84acc46f68ad
thxforthecode <ajbrown95@gmail.com>
2024-07-25 13:01:53 -0500

    Created details page. started report creation page. added CreateReportModel class.

---
+++ DowntimeAppUI/Models/CreateReportModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DowntimeAppUI.Models;
+
+public class CreateReportModel
+{
+   [Required]
+   [MaxLength(100)]
+   public string Issue { get; set; }
+
+   [Required]
+   [MaxLength(200)]
+   public string Solution { get; set; }
+
+   [Required]
+   [MinLength(1)]
+   [Display(Name = "Machine")]
+   public string MachineId { get; set; }
+}
